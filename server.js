2e90ff2e40ec324487cd9f4ea3c5a6ce79c96471
jerard <qbin221@gmail.com>
2020-02-09 15:34:06 -0500

    fixed heroku mongo connection

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ require("./routes/api")(app);
 require("./routes/html")(app);
 require("./routes/scrape")(app);
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/techScraper";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://newsScraper:password1@ds315359.mlab.com:15359/heroku_j53pz7h9";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
